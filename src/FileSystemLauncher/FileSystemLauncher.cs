baf82ee3c442ffe8a05c317209bc9e72083d9fd2
ema <mccoy39082@163.com>
2026-04-27 20:16:53 +0800

    Implement FileSystemLauncher open methods

--- src/FileSystemLauncher/FileSystemLauncher.cs
+++ src/FileSystemLauncher/FileSystemLauncher.cs
@@ -46,7 +46,38 @@ public static class FileSystemLauncher
 
     public static async Task OpenAsync(string path)
     {
-        // TODO
+        if (string.IsNullOrWhiteSpace(path)) throw new ArgumentException("Path must not be null or whitespace.", nameof(path));
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            string fullPath = path;
+            if (!Path.IsPathRooted(fullPath)) fullPath = Path.GetFullPath(fullPath);
+
+            bool launched;
+            try
+            {
+                launched = await Launcher.LaunchUriAsync(new Uri(fullPath)).ConfigureAwait(false);
+            }
+            catch
+            {
+                launched = false;
+            }
+
+            if (!launched)
+            {
+                ProcessStart("explorer.exe", $"\"{fullPath}\"");
+            }
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            ProcessStart("open", $"\"{path}\"");
+            await Task.CompletedTask;
+        }
+        else
+        {
+            ProcessStart("xdg-open", $"\"{path}\"");
+            await Task.CompletedTask;
+        }
     }
 
     public static void OpenFolder(string path)
@@ -56,7 +87,7 @@ public static class FileSystemLauncher
 
     public static async Task OpenFolderAsync(string path)
     {
-        // TODO
+        await OpenAsync(path).ConfigureAwait(false);
     }
 
     public static void OpenFolderAndSelectItem(string path)
@@ -75,7 +106,17 @@ public static class FileSystemLauncher
 
     public static async Task OpenFolderAndSelectItemAsync(string path)
     {
-        // TODO
+        if (string.IsNullOrWhiteSpace(path)) throw new ArgumentException("Path must not be null or whitespace.", nameof(path));
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            ProcessStart("explorer.exe", $"/select,\"{path}\"");
+            await Task.CompletedTask;
+        }
+        else
+        {
+            throw new NotSupportedException("Select item is only supported on Windows.");
+        }
     }
 
     private static void ProcessStart(string fileName, string arguments)
