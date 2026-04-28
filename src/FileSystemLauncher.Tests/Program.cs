0fe5da55545dabca9e4ad83602aa180df1723cb5
ema <mccoy39082@163.com>
2026-04-28 10:18:40 +0800

    Inline test calls in Program and remove helper

--- src/FileSystemLauncher.Tests/Program.cs
+++ src/FileSystemLauncher.Tests/Program.cs
@@ -2,25 +2,9 @@ namespace System.FileSystem.Tests;
 
 internal static class Program
 {
-    private static int Main(string[] args)
-    {
-        Console.WriteLine("FileSystemLauncher.Tests is now a console application.");
-
-        ShellServiceTests.OpenFolder_OnWindows_StartsExplorerWithQuotedPath();
-        ShellServiceTests.OpenFolderAndSelectItem_OnWindows_UsesSelectArg();
-        return 0;
-    }
-}
-
-public static class ShellServiceTests
-{
-    public static void OpenFolder_OnWindows_StartsExplorerWithQuotedPath()
+    private static void Main()
     {
         FileSystemLauncher.OpenFolder(@"C:\Program Files");
-    }
-
-    public static void OpenFolderAndSelectItem_OnWindows_UsesSelectArg()
-    {
         FileSystemLauncher.OpenFolderAndSelectItem(@"C:\Program Files\dotnet\dotnet.exe");
     }
 }
